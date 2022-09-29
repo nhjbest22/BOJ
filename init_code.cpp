e370a490be81db8f4da611f0e247d97456d56a7f
Seungmin Nam <nhjbest22@g.skku.edu>
2022-09-29 15:44:16 +0000

    dp 문제풀이 및 vscode 확장 추가

--- init_code.cpp
+++ init_code.cpp
@@ -76,21 +76,28 @@ bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
 #define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
 #define debug(x) cout << #x << " is " << x << '\n'
 
-int arr[100005];
-int d[100005]; //d[i] => i까지의 수열의 부분합 중 가장 큰 수, 단 i번째 수는 무조건 더해야 함.
-//d[i-1] + arr[i] 보다 arr[i]가 클 경우 굳이 부분합을 가져가지 않고 수열값만 가져가도 문제 x
+int arr[1005];
+int d[1005][10];
 
 int main (){
   ios::sync_with_stdio(0); cin.tie(0);
   int N; cin>>N;
-  rep(i,1,N+1){
-    cin>>arr[i];
+  rep(i,0,10){
+    d[1][i] = 1;
   }
-  d[1] = arr[1];
   rep(i,2,N+1){
-    d[i] = max(d[i-1]+ arr[i], arr[i]); 
+    rep(j,0,10){
+      rep(k,0,j+1){
+        d[i][j] += d[i-1][k];
+      }
+      d[i][j]%=10007;
+    }
   }
-  cout<<*(max_element(d+1, d+N+1));
+  int ans = 0;
+  rep(i,0,10){
+    ans += d[N][i];
+    ans%=10007;
+  }
+  cout<<ans;
   return 0;
-
 }
